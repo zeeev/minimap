e79c6770407e9a3df8e734f7bd6a0ca6fe9cb170
Zev N. Kronenberg <zevk@e51.grid.gs.washington.edu>
2016-10-04 11:01:14 -0700

    adding bed annotation

--- utils/plot/plotLayout.R
+++ utils/plot/plotLayout.R
@@ -50,8 +50,22 @@ option_list = list(
             default=10,
             help="The height of the plot",
             metavar="numeric"
+            ),
+            make_option(c("-t", "--title"),
+            type="character",
+            default="layout",
+            help="title of plot",
+            metavar="character"
+            ),
+            make_option(c("-b", "--bed"),
+            type="character",
+            default=NULL,
+            help="a bed file to paint on",
+            metavar="character"
             )
 );
+
+options(error=traceback)
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
@@ -67,13 +81,23 @@ tdat<-dat[dat$V17 > opt$dots,  ]
 
 vdat<-aggregate(dat$V5, by=list(dat$V4), max)
 
-thePlot<-ggplot(dat, aes(y=V2/1e6, yend=V3/1e6, x=V5/1e6, xend=V6/1e6, colour=V7))+geom_segment()
-thePlot<-thePlot+labs(x=opt$xlab, y=opt$ylab)
-thePlot<-thePlot+geom_point(data=tdat, aes(x=V5/1e6, y=V2/1e6), size=0.5)
-thePlot<-thePlot+geom_point(data=tdat, aes(x=V6/1e6, y=V3/1e6), size=0.5)
+thePlot<-ggplot()+geom_segment(data=dat,mapping=aes(y=V2/1e6, yend=V3/1e6, x=V5/1e6, xend=V6/1e6, colour=V7))
 thePlot<-thePlot+theme_classic()
 thePlot<-thePlot+theme(legend.position="none")
+
+thePlot<-thePlot+labs(x=opt$xlab, y=opt$ylab, title=opt$title)
+thePlot<-thePlot+geom_point(data=tdat, aes(x=V5/1e6, y=V2/1e6, colour=V7), size=0.5)
+thePlot<-thePlot+geom_point(data=tdat, aes(x=V6/1e6, y=V3/1e6, colour=V7), size=0.5)
+
 thePlot<-thePlot+geom_vline(xintercept=vdat$x/1e6, linetype="longdash", alpha=0.2)
+
+if (!is.null(opt$bed)){
+   bedDat<-read.table(opt$bed)
+   bedDat<-bedDat[ bedDat$V1 %in% tdat$V4,]
+
+   thePlot<-thePlot+geom_rect(data=bedDat, mapping=aes(ymin=0, ymax=max(dat$V3/1e6), xmin=V2/1e6, xmax=V3/1e6), alpha=0.25)
+}
+
 
 ggsave(filename=opt$pdf, width=opt$height, height=opt$height)
 
